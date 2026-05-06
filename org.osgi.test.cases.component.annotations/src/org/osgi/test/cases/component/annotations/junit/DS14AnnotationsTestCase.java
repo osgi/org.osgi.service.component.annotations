osgi/osgi

Add testcase with empty array and a class in component property types
Fix https://github.com/osgi/osgi/issues/646

org.osgi.test.cases.component.annotations/src/org/osgi/test/cases/component/annotations/junit/DS14AnnotationsTestCase.java
@@ -139,6 +139,27 @@ public class DS14AnnotationsTestCase extends AnnotationsTestCase {
 	}
 
 	@Test
+	public void testComponentPropertyTypesWithEmptyArray() throws Exception {
+		String name = testName.getMethodName();
+		Description description = descriptions.get(name);
+		assertThat(description).as(() -> {
+			try {
+				return toString(description.getElement());
+			} catch (Exception e) {
+				return e.toString();
+			}
+		})
+				.isNotNull()
+				.as("component %s", name)
+				.hasCount("property", 1)
+				.hasPropertyValue("adaptableClass", "String",
+						"java.lang.Runnable")
+				.doesNotContain("property[@name='adapterNames']")
+				.hasValue("implementation/@class",
+						"org.osgi.impl.bundle.component.annotations.ComponentPropertyTypesWithEmptyArray");
+	}
+
+	@Test
 	public void testActivationFields() throws Exception {
 		String name = testName.getMethodName();
 		Description description = descriptions.get(name);

osgi/org.osgi.service.component.annotations

Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>

org.osgi.test.cases.component.annotations/src/org/osgi/test/cases/component/annotations/junit/DS14AnnotationsTestCase.java
@@ -139,6 +139,27 @@ public class DS14AnnotationsTestCase extends AnnotationsTestCase {
 	}
 
 	@Test
+	public void testComponentPropertyTypesWithEmptyArray() throws Exception {
+		String name = testName.getMethodName();
+		Description description = descriptions.get(name);
+		assertThat(description).as(() -> {
+			try {
+				return toString(description.getElement());
+			} catch (Exception e) {
+				return e.toString();
+			}
+		})
+				.isNotNull()
+				.as("component %s", name)
+				.hasCount("property", 1)
+				.hasPropertyArrayValue("adaptableClass", "String",
+						"java.lang.Runnable")
+				.doesNotContain("property[@name='adapterNames']")
+				.hasValue("implementation/@class",
+						"org.osgi.impl.bundle.component.annotations.ComponentPropertyTypesWithEmptyArray");
+	}
+
+	@Test
 	public void testActivationFields() throws Exception {
 		String name = testName.getMethodName();
 		Description description = descriptions.get(name);